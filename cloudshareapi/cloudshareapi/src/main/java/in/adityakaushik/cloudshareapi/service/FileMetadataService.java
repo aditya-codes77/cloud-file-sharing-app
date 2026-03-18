d5c5a858d2616285f0ebb3880ecd8119e97ee194
aditya-codes77 <adityanarayankaushik47@gmail.com>
2026-03-18 17:28:07 +0530

    Fix missing Map import in FileMetadataService

--- cloudshareapi/cloudshareapi/src/main/java/in/adityakaushik/cloudshareapi/service/FileMetadataService.java
+++ cloudshareapi/cloudshareapi/src/main/java/in/adityakaushik/cloudshareapi/service/FileMetadataService.java
@@ -18,6 +18,7 @@ import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
